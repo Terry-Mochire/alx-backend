1fa1a14796acc5019542699bf5d64e4000aaee69
Terry <terrybmochire@gmail.com>
2023-01-26 00:51:04 +0300

    Task 1.

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,46 +1,40 @@
-#!/usr/bin/python3
-
-""" 1-main """
-
+#!/usr/bin/env python3
+""" BaseCaching module
+"""
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
     """
-        FIFOCache defines a FIFO caching system
-        Methods:
-            put(key, item) - store a key-value pair
-            get(key) - retrieve the value associated with a key
+    FIFOCache defines a FIFO caching system
     """
 
     def __init__(self):
         """
-            Initialize
+        Initialize the class with the parent's init method
         """
         super().__init__()
-        self.keys = []
+        self.order = []
 
     def put(self, key, item):
         """
-            Store a key-value pair
+        Cache a key-value pair
         """
-        if key is not None and item is not None:
-            if key in self.keys:
-                self.keys.remove(key)
-            self.keys.append(key)
-            self.cache_data[key] = item
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                discard = self.keys.pop(0)
-                del self.cache_data[discard]
-                print("DISCARD: {}".format(discard))
-        else:
+        if key is None or item is None:
             pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            self.order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """
-            Retrieve the value associated with a key
+        Return the value linked to a given key, or None
         """
-        if key is not None and key in self.cache_data:
+        if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
-        else:
-            return None
+        return None
