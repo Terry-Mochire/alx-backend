4d60a74d5924b94541699b174075bb43a7ef22c1
Terry <terrybmochire@gmail.com>
2023-02-01 01:10:41 +0300

    Task 0.

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+"""
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: returns the template '0-index.html'
+"""
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """Returns the template '0-index.html'"""
+    render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
