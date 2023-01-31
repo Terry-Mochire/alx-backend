1dd67fa957171660b2a8c2d68cbb575fad9cf09d
Terry <terrybmochire@gmail.com>
2023-02-01 01:14:29 +0300

    Task 0.

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,9 +12,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index() -> None:
     """Returns the template '0-index.html'"""
-    render_template('0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
